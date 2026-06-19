8bc1ac49bfc86e6515a55ae3b4ba13bf589c7f54
Christel-Mett <tl@mymadworld.de>
2026-06-19 22:19:20 +0200

    Nodemailer Sicherheitsupdate eingespielt und TLS-Fix ergänzt

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
-    }
+    },
+tls: {
+        rejectUnauthorized: false
+    }    
 });
 
 app.use(express.json());
